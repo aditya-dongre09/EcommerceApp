8f3a0c9a9c5264e8899e56919ed612a79f249265
Amogh Joshi <amoghdj@gmail.com>
2020-11-06 17:25:06 +0530

    Video 3 and 4 Done

--- app/src/main/java/com/example/ecommerceapp/SignInFragment.java
+++ app/src/main/java/com/example/ecommerceapp/SignInFragment.java
@@ -2,11 +2,16 @@ package com.example.ecommerceapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +33,8 @@ public class SignInFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private TextView dontHaveAnAccount;
+    private FrameLayout parentFrameLayout;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -59,6 +66,28 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        dontHaveAnAccount = view.findViewById(R.id.tv_dont_have_an_account);
+        parentFrameLayout = getActivity().findViewById(R.id.register_framelayout);
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        dontHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignUpFragment());
+            }
+        });
+    }
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slideout_from_left);
+        fragmentTransaction.replace(parentFrameLayout.getId(), fragment);
+        fragmentTransaction.commit();
+    }
+
 }
